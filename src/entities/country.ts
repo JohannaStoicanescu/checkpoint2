506229260e2f2e2af3da3bbd9d7dfc9a2b9ec574
Johanna <johanna.stoicanescu@gmail.com>
2024-04-15 15:10:19 +0200

    feature/update country entity and add createCountry method

--- src/entities/country.ts
+++ src/entities/country.ts
@@ -1,11 +1,51 @@
-import { BaseEntity, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Field, ID, ObjectType } from "type-graphql";
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { CreateCountry } from "../types/country.args";
 
 @Entity()
+@ObjectType()
 export class Country extends BaseEntity {
   @PrimaryGeneratedColumn()
+  @Field(() => ID)
   id!: number;
 
-  //   name!: string;
-  //   code!: string;
-  //   continent!: string;
+  @Field()
+  @Column()
+  code!: string;
+
+  @Field()
+  @Column()
+  name!: string;
+
+  @Field()
+  @Column()
+  emoji!: string;
+
+  @Field()
+  @Column()
+  continent!: string;
+
+  constructor(country?: CreateCountry) {
+    super();
+
+    if (country) {
+      if (!country.code) {
+        throw new Error("Code is required");
+      }
+      this.code = country.code;
+
+      if (!country.name) {
+        throw new Error("Name is required");
+      }
+      this.name = country.name;
+
+      if (!country.emoji) {
+        throw new Error("Emoji is required");
+      }
+      this.emoji = country.emoji;
+    }
+  }
+  static async createCountry(country?: CreateCountry) {
+    return new Country(country).save();
+  }
 }
