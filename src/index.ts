6b24b6a8e30605d5fc2ff2ef8c3c423cebfc383c
Johanna <johanna.stoicanescu@gmail.com>
2024-04-15 15:09:47 +0200

    feature/initialize apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
-const hello: string = "hello";
+import "reflect-metadata";
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { CountryResolver } from "./resolvers/countryResolver";
+import { buildSchema } from "type-graphql";
+import { getDataSource } from "./database";
 
-console.log(hello);
+const startApolloServer = async () => {
+  const schema = await buildSchema({ resolvers: [CountryResolver] });
+  const server = new ApolloServer({
+    schema,
+  });
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4001 },
+    context: async ({ req, res }) => ({ req, res }),
+  });
+  await getDataSource();
+
+  console.log(`🚀 Server ready at: ${url}`);
+};
+
+startApolloServer();
